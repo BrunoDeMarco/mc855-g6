df596ecacf72746d5e13fb6f524dcd1a5b0b589d
Bruno de Marco <brunodemarco@hotmail.com.br>
2022-06-20 00:03:43 -0300

    Adicionada uma primeira versão da página inicial

--- application/src/Home/Home.tsx
+++ application/src/Home/Home.tsx
@@ -1,5 +1,19 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
+import { CenteredPage } from "../components/CenteredPage/CenteredPage";
+import { Subtitle } from "../components/Subtitle/Subtitle";
+import { Title } from "../components/Title/Title";
 
 export const Home: React.FC = () => {
-  return <>Home</>;
+  return (
+    <CenteredPage>
+      <Title>Boas vindas ao Ambulatório de Pediatria do HC Unicamp!</Title>
+      <Subtitle>
+        Siga as etapas abaixo para realizar seu pré-atendimento e agilizar sua
+        consulta:
+      </Subtitle>
+      <TextField name={"senha"} value={""} label={"Digite sua senha aqui."} />
+      <Button variant={"contained"}>Próxima Etapa</Button>
+    </CenteredPage>
+  );
 };
---
+++ application/src/components/Subtitle/Subtitle.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Subtitle = styled.span(() => ({
+  fontSize: 16,
+}));
