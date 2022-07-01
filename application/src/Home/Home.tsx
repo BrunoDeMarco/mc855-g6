5442db522c83fa5c4a63f50d7aff26132519c7db
Bruno de Marco <brunodemarco@hotmail.com.br>
2022-06-30 23:12:28 -0300

    Adicionado um componente de layout para facilitar o uso de flexbox

--- application/src/Home/Home.tsx
+++ application/src/Home/Home.tsx
@@ -2,6 +2,7 @@ import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BaseLayout } from "../components/BaseLayout/BaseLayout";
+import { Box } from "../components/Box/Box";
 
 export const Home: React.FC = () => {
   return (
@@ -11,10 +12,12 @@ export const Home: React.FC = () => {
         "Siga as etapas abaixo para realizar seu pré-atendimento e agilizar sua consulta:"
       }
     >
-      <TextField name={"senha"} value={""} label={"Digite sua senha aqui."} />
-      <Link to={"/patient-type-selection"}>
-        <Button variant={"contained"}>Próxima Etapa</Button>
-      </Link>
+      <Box between={8}>
+        <TextField name={"senha"} value={""} label={"Digite sua senha aqui."} />
+        <Link to={"/patient-type-selection"}>
+          <Button variant={"contained"}>Próxima Etapa</Button>
+        </Link>
+      </Box>
     </BaseLayout>
   );
 };
