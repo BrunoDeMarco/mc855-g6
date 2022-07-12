2346faa7889e86733bf85afe65b68f4443caeda4
Lucas Antevere Santana <39019778+lucasasantana@users.noreply.github.com>
2022-07-11 23:23:37 -0300

    Criação de ambiente docker para server local

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import authRouter from "./controllers/authenticationController";
 import sampleProtectedRouter from "./controllers/sampleProtectedController";
+import userRouter from "./controllers/userController";
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(authRouter);
 app.use(sampleProtectedRouter);
+app.use(userRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
