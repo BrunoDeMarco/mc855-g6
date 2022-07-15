528e449d06511d16b9a1ac6bcf2e33a637526e14
Bruno de Marco <brunodemarco@hotmail.com.br>
2022-07-15 18:20:47 -0300

    Login fix

--- application/src/hospital/helpers/login.ts
+++ application/src/hospital/helpers/login.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const login = (username: string, password: string) => {
-  // TODO fix to call login api
-  // return axios.post("http://localhost:5050/session", { username, password });
+  return axios.post(`${process.env.REACT_APP_API_HOST}/session`, {
+    username,
+    password,
+  });
 };
